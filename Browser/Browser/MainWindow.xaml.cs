5632af58d38afe97631f2d5c3c170b5f285ca4f8
LeCheets <et.pitout@gmail.com>
2014-11-14 17:06:19 -0700

    tabs (well one tab)

--- Browser/Browser/MainWindow.xaml.cs
+++ Browser/Browser/MainWindow.xaml.cs
@@ -18,8 +18,13 @@ namespace Browser
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+
+        int openTab = 1;
+        WebBrowser curTab;
+        string curUrl = "";
         public MainWindow()
         {
             InitializeComponent();
@@ -63,6 +68,76 @@ namespace Browser
         private void settings_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void go_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (welcomeScreen.IsVisible)
+            {
+                welcomeScreen.Visibility = Visibility.Collapsed;
+                //browserScroll.Visibility = Visibility.Visible;
+                home.Visibility = Visibility.Visible;
+
+                //open the tab
+                if (openTab == 1)
+                {
+                    tab1.Visibility = Visibility.Visible;
+                    tab1.Navigate(urlbar.Text);
+                    tab1.Width = 1007;
+                    tab1.Height = 473;
+                    tab1.Margin = new Thickness(0, 42, 0, 0);
+                }
+            }
+        }
+
+        private void urlbar_GotFocus(object sender, RoutedEventArgs e)
+        {
+            urlbar.Text = "";
+        }
+
+        private void urlbar_LostFocus(object sender, RoutedEventArgs e)
+        {
+            //urlbar.Text = "Search or enter a URL here";
+        }
+
+        private void urlbar_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void home_Click_1(object sender, RoutedEventArgs e)
+        {
+            welcomeScreen.Visibility = Visibility.Visible;
+            //browser.Visibility = Visibility.Collapsed;
+            browserScroll.Visibility = Visibility.Collapsed;
+            home.Visibility = Visibility.Collapsed;
+            tab1but.Visibility = Visibility.Visible;
+
+            if (tab1.IsVisible)
+            {
+                welcome.Visibility = Visibility.Collapsed;
+                message1.Visibility = Visibility.Collapsed;
+
+                tab1.Margin = new Thickness(139, 92, 0, 0);
+                tab1.Height = 200;
+                tab1.Width = 343;
+            }
+        }
+
+        private void browser_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            urlbar.Text = e.Uri.ToString();
+            curUrl = e.Uri.ToString();
+        }
+
+        private void tab1but_Click(object sender, RoutedEventArgs e)
+        {
+            welcomeScreen.Visibility = Visibility.Collapsed;
+            home.Visibility = Visibility.Visible;
+
+            tab1.Width = 1007;
+            tab1.Height = 473;
+            tab1.Margin = new Thickness(0, 42, 0, 0);
         }
     }
 }
